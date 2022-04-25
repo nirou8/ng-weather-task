f32ce4753de90958ac8eb7c697cd1afc03a7ced4
nirou8 <khayati.nadhir@gmail.com>
2022-04-25 19:58:09 -0400

    patch - make the 3-state button accept ng-template type as input

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,25 +1,11 @@
-import { ChangeDetectorRef, Component, Input, QueryList, SimpleChanges, TemplateRef, ViewChildren } from "@angular/core";
-export const DEFAULT_STATE = 0
-export const WORKING_STATE = 1
-export const DONE_STATE = 2
-export const ERROR_STATE = 3
+import {Component, Input, TemplateRef } from "@angular/core";
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html'
 })
-export class ButtonComponent{
-    @ViewChildren('buttonStateTemplate') buttonStateTemplate: QueryList<TemplateRef<any>>;
-    @Input() buttonState: any;
-    selectedButtonStateTemplate: TemplateRef<any>
-    constructor(private changeDetectorRef: ChangeDetectorRef){}
-    ngAfterViewInit(): void {
-      this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[this.buttonState];
-      this.changeDetectorRef.detectChanges()
-    }
+export class ButtonComponent{ 
+    @Input() selectedButtonStateTemplate: TemplateRef<any>; 
+    constructor(){}
 
-    ngOnChanges(changes: SimpleChanges) {
-      if(!changes.buttonState.firstChange)
-        this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[this.buttonState]; 
-    }
-  
 }
--- src/app/zipcode-entry/zipcode-entry.component.ts
+++ src/app/zipcode-entry/zipcode-entry.component.ts
@@ -1,36 +1,47 @@
-import { Component } from '@angular/core';
-import { DEFAULT_STATE, DONE_STATE, ERROR_STATE, WORKING_STATE } from 'app/button/button.component';
+import { ChangeDetectorRef, Component, QueryList, TemplateRef, ViewChildren } from '@angular/core';
 import { WeatherService } from 'app/weather.service';
 import {LocationService} from "../location.service";
 
+// As you add new buttons states in the HTML you need to their relative states const here
+const DEFAULT_STATE = 0
+const WORKING_STATE = 1
+const DONE_STATE = 2
+const ERROR_STATE = 3
 @Component({
   selector: 'app-zipcode-entry',
   templateUrl: './zipcode-entry.component.html'
 })
 export class ZipcodeEntryComponent{
-  buttonState = DEFAULT_STATE
-  constructor(private service : LocationService, private weatherService : WeatherService) {
+  // We used ViewChildren here to avoid calling multiple @ViewChild
+  @ViewChildren('buttonStateTemplate') buttonStateTemplate: QueryList<TemplateRef<any>>;
+  selectedButtonStateTemplate: TemplateRef<any>
+  constructor(private service : LocationService, private weatherService : WeatherService, private changeDetectorRef: ChangeDetectorRef) {
 
    }
 
+   ngAfterViewInit(): void {
+    this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[DEFAULT_STATE];
+    this.changeDetectorRef.detectChanges()
+  }
+
   addLocation(zipcode : any, countrycode : any){
-    this.buttonState = WORKING_STATE
+    this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[WORKING_STATE];
     this.weatherService.getCurrentCondition(zipcode.value, countrycode.value).subscribe(
       data => {
         this.weatherService.pushCondition(zipcode.value, countrycode.value, data)
-       this.buttonState = DONE_STATE
+        this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[DONE_STATE];
         this.service.addLocation(zipcode.value, countrycode.value)
       },
       error =>{
         console.log(error)
-        this.buttonState = ERROR_STATE
+        this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[ERROR_STATE];
       }
       );
 
       setTimeout(() => {
         zipcode.value = ''
         countrycode.value = ''
-        this.buttonState = DEFAULT_STATE
+        this.selectedButtonStateTemplate = this.buttonStateTemplate.toArray()[DEFAULT_STATE];
       }, 500)
 
   }
